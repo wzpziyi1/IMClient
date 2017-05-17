fc18c6880ede7eb3535ad0b5a2311ba718fa8239
wzpziyi1 <1204473090@qq.com>
2017-05-17 23:36:54 +0800

    引入ysocket

--- Client/ZYSocket/ZYSocket.swift
+++ Client/ZYSocket/ZYSocket.swift
@@ -8,6 +8,36 @@
 
 import UIKit
 
-class ZYSocket: NSObject {
+class ZYSocket {
+    
+    fileprivate var client: TCPClient
+    
+    init(addr: String, port: Int) {
+        client = TCPClient(addr: addr, port: port)
+    }
+    
+}
 
+extension ZYSocket {
+    
+    /// 连接服务器
+    ///
+    func connectServe() -> Bool {
+        return client.connect(timeout: 5).0
+    }
+    
+    func readMessage() {
+        
+        DispatchQueue.global().async {
+            while true {
+                //读取4个字节的head长度，读出来的是后续这个真实字节流的长度
+                guard let msgLen = self.client.read(4) else {
+                    continue
+                }
+                
+//                let msg =
+            }
+        }
+        
+    }
 }
